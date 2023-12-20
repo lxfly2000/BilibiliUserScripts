c3073f8f2960e5e4458d5ced650e7fef3d93186a
Yuxin <lxfly2000@outlook.com>
2023-12-20 10:29:34 +0800

    Update 哔哩哔哩计数器.user.js

--- 哔哩哔哩计数器.user.js
+++ 哔哩哔哩计数器.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         哔哩哔哩计数器
 // @namespace    https://github.com/lxfly2000/BilibiliCounter/raw/master/哔哩哔哩计数器.user.js
-// @version      2.0.3
+// @version      2.0.4
 // @updateURL    https://github.com/lxfly2000/BilibiliCounter/raw/master/哔哩哔哩计数器.user.js
 // @downloadURL  https://github.com/lxfly2000/BilibiliCounter/raw/master/哔哩哔哩计数器.user.js
 // @description  显示哔哩哔哩上传视频数的实际计数
@@ -17,8 +17,8 @@
     addEventListener("load",bilibiliCounter);//等页面加载完后再执行否则会被覆盖
 })();
 
-var tagListItem={};//分区名（网页上）——DOM节点
-var tagNameTable={};//tid与分区名（ZoneConfig定义）转换表
+var tagListItem={};//分区名（网页上）——DOM节点；格式为{"分区名（中文）":DOM节点,...}
+var tagNameTable={};//tid与分区名（ZoneConfig定义）转换表；格式为{分区ID:Set集合{"分区名（简体中文）","分区名（繁体中文）",...},...}
 
 function bilibiliCounter(){
     getHomeLink();
@@ -26,12 +26,15 @@ function bilibiliCounter(){
 
 function getHomeLink(){
     var xhr=new XMLHttpRequest();
-    xhr.open("GET","//www.bilibili.com",true);
+    //2023-12-20：首页的计数器已不再显示，但是虚拟主播页面还有
+    xhr.open("GET","//www.bilibili.com/v/virtual/",true);
     xhr.onreadystatechange=function(){
         if(xhr.readyState==4&&xhr.status==200){
             var homelink="";
             //2020-4-5：首页被替换为新版主页
-            var homereg=new RegExp("//s[0-9]\\.hdslb\\.com/bfs/static/jinkela/international-home/international-home\\.?[^\\.]+\\.js");
+            //2023-12-20：首页的计数器已不再显示
+            //2023-12-20：备用链接：https://s1.hdslb.com/bfs/seed/laputa-header/bili-header.umd.js（用来获取分区名与ID的配置）
+            var homereg=new RegExp("//s[0-9]\\.hdslb\\.com/bfs/static/jinkela/channel/channel\\.?[^\\.]+\\.js");
             var regMatch=xhr.responseText.match(homereg);
             if(regMatch==null){
                 console.log("无法找到对应的脚本URL。");
@@ -96,8 +99,22 @@ function writeCounter(jsonData){
             }
         }
     }catch(e){
-        console.log("未找到DOM子节点。");
-        return;
+        console.log("未找到DOM子节点，正在尝试按2023年版页面查找……");
+        try{
+            doms=document.getElementsByClassName("channel-link");
+            for(i=0;i<doms.length;i++){
+                try{
+                    var sdom=document.createElement("span");
+                    sdom.style.letterSpacing="0px";
+                    doms[i].appendChild(sdom);
+                    tagListItem[doms[i].textContent]=sdom;
+                }catch(e){
+                    console.log("建立节点表时错误。"+e.message);
+                }
+            }
+        }catch(e){
+            console.log("未找到DOM子节点。");
+        }
     }
 
     var counterData=jsonData.data.region_count;
