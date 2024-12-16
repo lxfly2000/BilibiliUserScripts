c012e41841e726bb19c3a5c37967a7f81f9d9e0e
Yuxin <lxfly2000@outlook.com>
2024-12-16 21:07:49 +0800

    Update 自动展开弹幕列表.user.js

--- 自动展开弹幕列表.user.js
+++ 自动展开弹幕列表.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动展开弹幕列表
 // @namespace    https://github.com/lxfly2000/BilibiliUserScripts/raw/master/自动展开弹幕列表.user.js
-// @version      1.0.1
+// @version      1.0.2
 // @updateURL    https://github.com/lxfly2000/BilibiliUserScripts/raw/master/自动展开弹幕列表.user.js
 // @downloadURL  https://github.com/lxfly2000/BilibiliUserScripts/raw/master/自动展开弹幕列表.user.js
 // @description  自动展开弹幕列表
@@ -11,17 +11,31 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+let BilibiliAutoUnfoldDanmakuList_mo=new MutationObserver(mutations=>BilibiliAutoUnfoldDanmakuList_Main());
+
+function BilibiliAutoUnfoldDanmakuList_StartObserve(){
+    BilibiliAutoUnfoldDanmakuList_mo.observe(document.getRootNode(),{attributes:false,childList:false,characterData:true,subtree:true});
+}
+
+function BilibiliAutoUnfoldDanmakuList_StopObserve(){
+    BilibiliAutoUnfoldDanmakuList_mo.disconnect();
+}
+
+function BilibiliAutoUnfoldDanmakuList_Main(){
     "use strict";
-
-    addEventListener("load",bilibiliAutoUnfoldDanmakuList);//等页面加载完后再执行
-})();
-
-function bilibiliAutoUnfoldDanmakuList(){
+    BilibiliAutoUnfoldDanmakuList_StopObserve();
     var headers=document.getElementsByClassName('bui-collapse-header');
     for(var h of headers){
         if(h.parentElement.classList.contains('bui-collapse-wrap-folded')){
             h.click();
+            console.log("展开弹幕列表窗口…");
         }
     }
+    BilibiliAutoUnfoldDanmakuList_StartObserve();
 }
+
+(function() {
+    "use strict";
+
+    BilibiliAutoUnfoldDanmakuList_Main();
+})();
